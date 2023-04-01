4d757cfde62955fc1dfb97fb4534e69bac88115e
Nycollas_Sobolevski <sobolevski.academy@gmail.com>
2023-04-01 09:58:46 -0300

    Update to save

--- bot.py
+++ bot.py
@@ -3,17 +3,37 @@ import secretos
 import urbs
 
 bot = telebot.TeleBot(secretos.api_key)
+resposta = ''
 
+def apel(bot, update):
+    msg = update.message.reply_text('Qual o seu apelido?')
+    bot.register_next_step_handler(msg, nome_de_uma_funcao)
+    bot.send_message(chat_id = update.message.chat_id, text = f"Uau, seu apelido é {resposta}" )
+
+
+def nome_de_uma_funcao(message):
+# Guardando a resposta do usuario em uma variavel
+    resposta = message.text
+    print(resposta)
+cpf =''
 @bot.message_handler(commands=["opcao1"])
 def Saldo(mensagem):
     bot.send_message(mensagem.chat.id, f"Digite seu CPF")
+
+
     # aprender como receber uma mensagem
     #TODO: 1puxar funcao de consultar saldo
-    if secretos.certify_cpf('11370672926'):
-        saldo = urbs.saldo('11370672926')
+    if secretos.certify_cpf(cpf):
+        saldo = urbs.saldo(cpf)
         bot.send_message(mensagem.chat.id, f"{saldo[len(saldo)-1]} seu saldo é de {saldo[:3]}")
     else:
         bot.send_message(mensagem.chat.id, f"CPF invalido no banco de dados, digite novamente ou contate o ADMIN")
+
+
+def Save_cpf(message):
+  # Guardando a resposta do usuario em uma variavel
+  cpf = message.text
+
 
 
 
